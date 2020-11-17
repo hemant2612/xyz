536cecec3387a181b890ba6e3d0005f8dfb422db
hemant gaur <hemant.gaur_cs18@gla.ac.in>
2020-11-17 15:06:49 +0530

    again change admin

--- routes/users.js
+++ routes/users.js
@@ -66,7 +66,7 @@ router.post('/register', (req, res) => {
                     username: username,
                     password: password,
                     age: age,
-                    admin: 1
+                    admin: 0
                 });
                 bcrypt.hash(user.password, 10, function (err, hash) {
                     if (err)
